60f4dc12a53d0ec54ab2caa1ad8b9d00ab22ec8b
Tom Black <tom.black06007@gmail.com>
2020-08-25 18:17:26 -0700

    Added some components.

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Do Dishes",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Go for run",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Read book",
+        completed: false
+      }
+    ]
+  }
+  
+  render () {
+    return (
+      <div className="App">
+        <Todos />
+      </div>
+    );
+  }
 }
 
 export default App;
