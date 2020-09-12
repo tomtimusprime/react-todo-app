9be41d11e9b65b3c0792f503481bbf044c0723f3
Tom Black <tom.black06007@gmail.com>
2020-09-12 13:02:27 -0700

    Got the delete todo functionality working.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class TodoItem extends Component {
     getStyle = () => {
@@ -21,13 +21,22 @@ export class TodoItem extends Component {
                 <p>
                     <input type="checkbox" onChange={this.props.markComplete.bind(this, id)}/>
                     {title}
-                    <button style={btnStyle}>x</button>
+                    <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
                 </p>
             </div>
         )
     }
 }
 
-const btnStyle          
+const btnStyle = {
+    background: "#ff0000",
+    color: "#fff",
+    border: "none",
+    padding: "5px 10px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right"
+
+}         
 
 export default TodoItem;
